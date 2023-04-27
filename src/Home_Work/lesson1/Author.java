94fff88d47ce9e9c695c490c18e5be8a0da35801
Yurii <shadowjura82@gmail.com>
2023-04-27 15:40:17 +0200

    Home work 2 has been completed

--- src/Home_Work/lesson1/Author.java
+++ src/Home_Work/lesson1/Author.java
@@ -1,5 +1,7 @@
 package Home_Work.lesson1;
 
+import java.util.Objects;
+
 public class Author {
     private String firstName;
     private String lastName;
@@ -15,5 +17,29 @@ public class Author {
 
     public String getLastName() {
         return this.lastName;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Author author = (Author) o;
+
+        // Вариант сгенерированный автоматически
+        // return Objects.equals(firstName, author.firstName) && Objects.equals(lastName, author.lastName);
+
+        // Мой вариант
+        // Можно использовать такой вариант? Если да, то в чем разница между моим и автоматически сгенерированным?
+        return firstName.equals(author.firstName) && lastName.equals(author.lastName);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(firstName, lastName);
+    }
+
+    @Override
+    public String toString() {
+        return "Автор: " + firstName + " " + lastName;
     }
 }
--- src/Home_Work/lesson1/Book.java
+++ src/Home_Work/lesson1/Book.java
@@ -1,5 +1,7 @@
 package Home_Work.lesson1;
 
+import java.util.Objects;
+
 public class Book {
     private String bookName;
     private Author bookAuthor;
@@ -25,5 +27,30 @@ public class Book {
 
     public int getBookYear() {
         return this.bookYear;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Book book = (Book) o;
+        // Вариант сгенерированный автоматически
+        //return bookYear == book.bookYear && Objects.equals(bookName, book.bookName) && Objects.equals(bookAuthor, book.bookAuthor);
+
+        // Мой вариант
+        // Можно использовать такой вариант? Если да, то в чем разница между моим и автоматически сгенерированным?
+        return bookYear == book.bookYear && bookName.equals(book.bookName) && bookAuthor.equals(book.bookAuthor);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(bookName, bookAuthor, bookYear);
+    }
+
+    @Override
+    public String toString() {
+        return "Книга: " + bookName +
+                "\nАвтор: " + bookAuthor +
+                "\nГод выпуска: " + bookYear;
     }
 }
